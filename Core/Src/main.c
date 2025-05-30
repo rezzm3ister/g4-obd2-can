1889531ac45309a66a43fa1c6cc94a6f965d24c1
rezzm3ister <mlpa16.2@gmail.com>
2025-05-31 00:49:47 +0800

    fixed tim1 prescaler

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -226,7 +226,7 @@ static void MX_TIM1_Init(void)
 
   /* USER CODE END TIM1_Init 1 */
   htim1.Instance = TIM1;
-  htim1.Init.Prescaler = 170;
+  htim1.Init.Prescaler = 169;
   htim1.Init.CounterMode = TIM_COUNTERMODE_UP;
   htim1.Init.Period = 99;
   htim1.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
